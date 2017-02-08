4acaf58174afa9c04c5b0812d446ff556bbadb7c
maplesauce <garfieldknowsbest@gmail.com>
2017-02-08 17:40:16 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# SimpleRepair
+Plugin implementing a command that repairs all repairable items and armor in your inventory.
---
+++ src/main/java/net/maplesauce/simplerepair/RepairPlugin.java
@@ -0,0 +1,40 @@
+package net.maplesauce.simplerepair;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Repairable;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RepairPlugin extends JavaPlugin {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) return false;
+
+        Player player = (Player) sender;
+        if (command.getName().equalsIgnoreCase("repair")) {
+            if (!player.hasPermission("simplerepair.use")) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to perform this command.");
+                return true;
+            }
+            repairAll(player);
+            player.sendMessage(ChatColor.GREEN + "You have repaired your tools and armor!");
+        }
+        return false;
+    }
+
+    private void repairAll(Player player) {
+        for (ItemStack items : player.getInventory().getContents()) {
+            if (items instanceof Repairable) {
+                items.setDurability((short)0);
+            }
+        }
+        for (ItemStack items : player.getInventory().getArmorContents()) {
+            if (items instanceof Repairable) {
+                items.setDurability((short)0);
+            }
+        }
+    }
+}
